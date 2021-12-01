7e246b0d47d326c221cb3cf0a90f8827a3fa0f66
Filipe Ribeiro <filipepaiva240@gmail.com>
2021-11-30 21:43:10 -0300

    Add delete account route

--- src/index.js
+++ src/index.js
@@ -119,6 +119,14 @@ app.get("/account", verifyCpfExistence, (request, response) => {
   const { customer } = request;
 
   return response.json(customer);
-})
+});
+
+app.delete("/account", verifyCpfExistence, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
 
 app.listen(3333);
